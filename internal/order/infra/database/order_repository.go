04617eabfc6541a102bef410a5bcc66d700562b5
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-09-26 22:32:06 -0300

    first commit

---
+++ internal/order/infra/database/order_repository.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/igorsbezerra/pfa-go/internal/order/entity"
+)
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository {
+	return &OrderRepository{Db: db}
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	stms, err := r.Db.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	_, err = stms.Exec(order.ID, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+	return nil
+}
